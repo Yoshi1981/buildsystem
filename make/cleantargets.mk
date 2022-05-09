34380aa798f49d2cd9d06ac6fd5714754304c05e
mohousch <mohousch@gmail.com>
2022-05-09 13:25:40 +0200

    - cleanup.

--- make/cleantargets.mk
+++ make/cleantargets.mk
@@ -8,7 +8,7 @@ clean:
 	@-$(MAKE) tools-clean
 	@-rm -rf $(RELEASE_DIR)
 	@-rm -rf $(TARGET_DIR)
-#	@-rm -rf $(HOST_DIR)
+	@-rm -rf $(SOURCE_DIR)
 	@-rm -rf $(BOOT_DIR)
 	@-rm -rf $(D)/kernel
 	@-rm -rf $(D)/*.do_compile
@@ -17,18 +17,10 @@ clean:
 	@-rm -rf $(D)/directories
 	@-rm -rf $(D)/diverse-tools
 	@-rm -rf $(D)/system-tools
-#	@-rm -rf $(D)/*
-#ifeq ($(BOXARCH),sh4)	
-#	touch $(D)/crosstool
-#	touch $(D)/crosstool-rpminstall
-#endif
 	@echo -e "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
 
 distclean: depsclean
 	@echo -e "$(TERM_YELLOW)---> cleaning whole build system .. $(TERM_NORMAL)"
-#	@-$(MAKE) kernel-clean
-#	@-$(MAKE) driver-clean
-#	@-rm -rf $(TUFSBOX_DIR)
 	@-rm -rf $(RELEASE_DIR)
 	@-rm -rf $(TARGET_DIR)
 	@-rm -rf $(HOST_DIR)
