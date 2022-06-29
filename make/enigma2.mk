a351159776bb3e2f5b2b3763b9309bac39c72f26
mohousch <mohousch@gmail.com>
2022-06-29 11:15:32 +0200

    - update.

--- make/enigma2.mk
+++ make/enigma2.mk
@@ -18,7 +18,7 @@ ENIGMA2_DEPS += $(D)/tuxtxt32bpp
 ENIGMA2_DEPS += $(D)/hotplug_e2_helper
 ENIGMA2_DEPS += $(D)/avahi
 ENIGMA2_DEPS += python-all
-ifneq ($(OPTIMIZATIONS), $(filter $(OPTIMIZATIONS), small))
+ifneq ($(OPTIMIZATIONS), $(filter $(OPTIMIZATIONS), size))
 ENIGMA2_DEPS += $(D)/ethtool
 ENIGMA2_DEPS += $(D)/alsa_utils
 ENIGMA2_DEPS += $(D)/libdreamdvd
@@ -36,57 +36,59 @@ endif
 
 ENIGMA2_DEPS  += $(D)/libsigc
 
+E2_CONFIG_OPTS =
+
 #ifeq ($(MEDIAFW), gstreamer)
 ENIGMA2_DEPS  += $(D)/gstreamer $(D)/gst_plugins_base $(D)/gst_plugins_dvbmediasink
 ENIGMA2_DEPS  += $(D)/gst_plugins_good $(D)/gst_plugins_bad $(D)/gst_plugins_ugly
-E_CONFIG_OPTS += --with-gstversion=1.0 --enable-mediafwgstreamer
+E2_CONFIG_OPTS += --with-gstversion=1.0 --enable-mediafwgstreamer
 #endif
 
 ifeq ($(EXTERNAL_LCD), graphlcd)
-E_CONFIG_OPTS += --with-graphlcd
+E2_CONFIG_OPTS += --with-graphlcd
 ENIGMA2_DEPS_ += $(D)/graphlcd
 endif
 
 ifeq ($(EXTERNAL_LCD), lcd4linux)
-E_CONFIG_OPTS += --with-lcd4linux
+E2_CONFIG_OPTS += --with-lcd4linux
 ENIGMA2_DEPS += $(D)/lcd4linux
 endif
 
 ifeq ($(GRAPHLCD), graphlcd)
-E_CONFIG_OPTS += --with-graphlcd
+E2_CONFIG_OPTS += --with-graphlcd
 ENIGMA2_DEPS_ += $(D)/graphlcd
 endif
 
 ifeq ($(LCD4LINUX), lcd4linux)
-E_CONFIG_OPTS += --with-lcd4linux
+E2_CONFIG_OPTS += --with-lcd4linux
 ENIGMA2_DEPS += $(D)/lcd4linux
 endif
 
-E_CPPFLAGS    = -I$(DRIVER_DIR)/include
-E_CPPFLAGS   += -I$(TARGET_DIR)/usr/include
-E_CPPFLAGS   += -I$(KERNEL_DIR)/include
-E_CPPFLAGS   += -I$(APPS_DIR)/tools
+E2_CPPFLAGS    = -I$(DRIVER_DIR)/include
+E2_CPPFLAGS   += -I$(TARGET_DIR)/usr/include
+E2_CPPFLAGS   += -I$(KERNEL_DIR)/include
+E2_CPPFLAGS   += -I$(APPS_DIR)/tools
 
-E_CONFIG_OPTS += PYTHON_CPPFLAGS="-I$(TARGET_DIR)/usr/include/python2.7" PYTHON_LIBS="-L$(TARGET_DIR)/usr/lib -lpython2.7" PYTHON_SITE_PKG="$(TARGET_DIR)/usr/lib/python2.7/site-packages"
+E2_CONFIG_OPTS += PYTHON_CPPFLAGS="-I$(TARGET_DIR)/usr/include/python2.7" PYTHON_LIBS="-L$(TARGET_DIR)/usr/lib -lpython2.7" PYTHON_SITE_PKG="$(TARGET_DIR)/usr/lib/python2.7/site-packages"
 
 ENIGMA2_PATCHES =
 
 #
 # enigma2
 #
-$(D)/enigma2.do_prepare: | $(ENIGMA2_DEPS)
+$(D)/enigma2.do_prepare: $(ENIGMA2_DEPS)
 	$(START_BUILD)
 	rm -rf $(SOURCE_DIR)/enigma2
 	[ -d "$(ARCHIVE)/enigma2.git" ] && \
 	(cd $(ARCHIVE)/enigma2.git; git pull;); \
 	[ -d "$(ARCHIVE)/enigma2.git" ] || \
-	git clone -b 6.4 https://github.com/openatv/enigma2.git $(ARCHIVE)/enigma2.git; \
+	git clone https://github.com/OpenVisionE2/enigma2-openvision-sh4.git $(ARCHIVE)/enigma2.git; \
 	cp -ra $(ARCHIVE)/enigma2.git $(SOURCE_DIR)/enigma2; \
 	set -e; cd $(SOURCE_DIR)/enigma2; \
 		$(call apply_patches,$(ENIGMA2_PATCHES))
 	@touch $@
 
-$(SOURCE_DIR)/enigma2/config.status: $(D)/enigma2.do_prepare
+$(D)/enigma2.config.status: $(D)/enigma2.do_prepare
 	$(SILENT)cd $(SOURCE_DIR)/enigma2; \
 		./autogen.sh $(SILENT_OPT); \
 		sed -e 's|#!/usr/bin/python|#!$(HOST_DIR)/bin/python|' -i po/xml2po.py; \
@@ -94,7 +96,7 @@ $(SOURCE_DIR)/enigma2/config.status: $(D)/enigma2.do_prepare
 		./configure $(SILENT_CONFIGURE) \
 			--build=$(BUILD) \
 			--host=$(TARGET) \
-			$(E_CONFIG_OPTS) \
+			$(E2_CONFIG_OPTS) \
 			--with-libsdl=no \
 			--datadir=/usr/local/share \
 			--libdir=/usr/lib \
@@ -102,46 +104,19 @@ $(SOURCE_DIR)/enigma2/config.status: $(D)/enigma2.do_prepare
 			--prefix=/usr \
 			--sysconfdir=/etc \
 			--with-boxtype=$(BOXTYPE) \
-			$(ENIGMA_OPT_OPTION) \
 			PKG_CONFIG=$(PKG_CONFIG) \
 			PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
 			PY_PATH=$(TARGET_DIR)/usr \
-			CPPFLAGS="$(E_CPPFLAGS)"
+			CPPFLAGS="$(E2_CPPFLAGS)"
+	@touch $@
 
-$(D)/enigma2.do_compile: $(SOURCE_DIR)/enigma2/config.status
+$(D)/enigma2.do_compile: $(D)/enigma2.config.status
 	$(SILENT)cd $(SOURCE_DIR)/enigma2; \
 		$(MAKE) all
 	@touch $@
 
-PLI_SKIN_PATCH = PLi-HD_skin.patch
-REPO_PLIHD="https://github.com/littlesat/skin-PLiHD.git"
-HEAD=master
-#REVISION_HD=8c9e43bd5b5fbec2d0e0e86d8e9d69a94f139054
-REPO_0=$(REPO_PLIHD)
-FW=$(MEDIAFW)
-$(D)/enigma2: $(D)/enigma2.do_prepare $(D)/enigma2.do_compile
+$(D)/enigma2: $(D)/enigma2.do_compile
 	$(MAKE) -C $(SOURCE_DIR)/enigma2 install DESTDIR=$(TARGET_DIR)
-#	@echo -n "Stripping..."
-#	$(SILENT)if [ -e $(TARGET_DIR)/usr/bin/enigma2 ]; then \
-#		$(TARGET)-strip $(TARGET_DIR)/usr/bin/enigma2; \
-#	fi
-#	$(SILENT)if [ -e $(TARGET_DIR)/usr/local/bin/enigma2 ]; then \
-#		$(TARGET)-strip $(TARGET_DIR)/usr/local/bin/enigma2; \
-#	fi
-#	$(SILENT)echo " done."
-#	$(SILENT)echo
-#	$(SILENT)echo "Adding PLi-HD skin"
-#	$(SILENT)if [ ! -d $(ARCHIVE)/PLi-HD_skin.git ]; then \
-#		(echo -n "Cloning PLi-HD skin git..."; git clone -q -b $(HEAD) $(REPO_0) $(ARCHIVE)/PLi-HD_skin.git; echo " done."); \
-#	fi
-#	$(SILENT)(cd $(ARCHIVE)/PLi-HD_skin.git; echo -n "Checkout commit $(REVISION_HD)..."; git checkout -q $(REVISION_HD); echo " done.")
-#	$(SILENT)cp -ra $(ARCHIVE)/PLi-HD_skin.git/usr/share/enigma2/* $(TARGET_DIR)/usr/local/share/enigma2
-#	@echo -e "$(TERM_RED)Applying Patch:$(TERM_NORMAL) $(PLI_SKIN_PATCH)"; $(PATCH)/$(PLI_SKIN_PATCH)
-#	@echo -e "Patching $(TERM_GREEN_BOLD)PLi-HD skin$(TERM_NORMAL) completed."
-#ifneq ($(BOXTYPE), $(filter $(BOXTYPE), spark spark7162 cuberevo cuberevo_250hd cuberevo_mini_fta cuberevo_mini cuberevo_mini2 cuberevo_2000hd cuberevo3000hd cuberevo_9500hd fs9000 hs7110 hs7420 hs7810a hs7119 hs7429 hs7819 hs8200 hs9510 tf7700 ufs912 ufs913))
-#	$(SILENT)rm -rf $(TARGET_DIR)/usr/local/share/enigma2/PLi-FullHD
-#	$(SILENT)rm -rf $(TARGET_DIR)/usr/local/share/enigma2/PLi-FullNightHD
-#endif
 	$(TOUCH)
 
 enigma2-clean:
