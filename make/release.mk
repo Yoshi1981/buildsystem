5cd7588b69017ed4433f5f9ef9fa6ab9128ca055
mohousch <mohousch@gmail.com>
2022-06-30 07:32:34 +0200

    - finally got it fix build titan for sh4.

--- make/release.mk
+++ make/release.mk
@@ -51,7 +51,11 @@ release-common: $(RELEASE_DEPS)
 	cp -aR $(SKEL_ROOT)/usr/lib/locale/* $(RELEASE_DIR)/usr/lib/locale
 	install -d $(RELEASE_DIR)/usr/local/{bin,sbin,share}
 ifeq ($(FLAVOUR), TITAN)
-	install -d $(RELEASE_DIR)/usr/local/share/skin
+	install -d $(RELEASE_DIR)/var/etc/titan
+	install -d $(RELEASE_DIR)/var/etc/autostart
+	install -d $(RELEASE_DIR)/var/usr/local/share/titan/skin
+	install -d $(RELEASE_DIR)/var/usr/local/share/titan/po
+	install -d $(RELEASE_DIR)/var/usr/share/fonts
 endif
 	install -d $(RELEASE_DIR)/usr/share/{tuxbox,udhcpc,zoneinfo,lua,fonts,iso-codes}
 	install -d $(RELEASE_DIR)/usr/share/lua/5.2
@@ -229,19 +233,6 @@ endif
 # modules.available
 #
 	cp -aR $(SKEL_ROOT)/etc/modules.available_$(BOXARCH) $(RELEASE_DIR)/etc/modules.available
-#
-# lib usr/lib
-#
-	cp -R $(TARGET_DIR)/lib/* $(RELEASE_DIR)/lib/
-	rm -f $(RELEASE_DIR)/lib/*.{a,o,la}
-	chmod 755 $(RELEASE_DIR)/lib/*
-
-	cp -R $(TARGET_DIR)/usr/lib/* $(RELEASE_DIR)/usr/lib/
-
-	rm -rf $(RELEASE_DIR)/usr/lib/{engines,gconv,libxslt-plugins,pkgconfig,sigc++-2.0,python*,lua}
-	rm -f $(RELEASE_DIR)/usr/lib/*.{a,o,la}
-
-	chmod 755 $(RELEASE_DIR)/usr/lib/*
 
 #
 # mc
@@ -341,53 +332,6 @@ endif
 	fi
 
 #
-# delete unnecessary files
-#
-ifeq ($(BOXTYPE), $(filter $(BOXTYPE), ufs910 ufs922))
-	rm -f $(RELEASE_DIR)/sbin/jfs_fsck
-	rm -f $(RELEASE_DIR)/sbin/fsck.jfs
-	rm -f $(RELEASE_DIR)/sbin/jfs_mkfs
-	rm -f $(RELEASE_DIR)/sbin/mkfs.jfs
-	rm -f $(RELEASE_DIR)/sbin/jfs_tune
-	rm -f $(RELEASE_DIR)/sbin/ffmpeg
-	rm -f $(RELEASE_DIR)/etc/ssl/certs/ca-certificates.crt
-endif
-	rm -rf $(RELEASE_DIR)/lib/autofs
-	rm -f $(RELEASE_DIR)/lib/libSegFault*
-	rm -f $(RELEASE_DIR)/lib/libstdc++.*-gdb.py
-	rm -f $(RELEASE_DIR)/lib/libthread_db*
-	rm -f $(RELEASE_DIR)/lib/libanl*
-	rm -rf $(RELEASE_DIR)/lib/modules/$(KERNEL_VER)
-	rm -rf $(RELEASE_DIR)/usr/lib/alsa
-	rm -rf $(RELEASE_DIR)/usr/lib/glib-2.0
-	rm -rf $(RELEASE_DIR)/usr/lib/cmake
-	rm -f $(RELEASE_DIR)/usr/lib/*.py
-	rm -f $(RELEASE_DIR)/usr/lib/libc.so
-	rm -f $(RELEASE_DIR)/usr/lib/xml2Conf.sh
-	rm -f $(RELEASE_DIR)/usr/lib/libfontconfig*
-	rm -f $(RELEASE_DIR)/usr/lib/libdvdcss*
-	rm -f $(RELEASE_DIR)/usr/lib/libdvdnav*
-	rm -f $(RELEASE_DIR)/usr/lib/libdvdread*
-	rm -f $(RELEASE_DIR)/usr/lib/libcurses.so
-	[ ! -e $(RELEASE_DIR)/usr/bin/mc ] && rm -f $(RELEASE_DIR)/usr/lib/libncurses* || true
-	rm -f $(RELEASE_DIR)/usr/lib/libthread_db*
-	rm -f $(RELEASE_DIR)/usr/lib/libanl*
-	rm -f $(RELEASE_DIR)/usr/lib/libopkg*
-	rm -f $(RELEASE_DIR)/bin/gitVCInfo
-	rm -f $(RELEASE_DIR)/bin/evtest
-	rm -f $(RELEASE_DIR)/bin/meta
-	rm -f $(RELEASE_DIR)/bin/streamproxy
-	rm -f $(RELEASE_DIR)/bin/libstb-hal-test
-	rm -f $(RELEASE_DIR)/sbin/ldconfig
-	rm -f $(RELEASE_DIR)/usr/bin/pic2m2v
-	rm -f $(RELEASE_DIR)/usr/bin/{gdbus-codegen,glib-*,gtester-report}
-ifeq ($(BOXARCH), $(filter $(BOXARCH), arm mips))
-	rm -rf $(RELEASE_DIR)/dev.static
-	rm -rf $(RELEASE_DIR)/ram
-	rm -rf $(RELEASE_DIR)/root
-endif
-
-#
 # lua
 #
 ifeq ($(LUA), lua)
@@ -464,6 +408,70 @@ $(D)/release-NONE: release-common release-$(BOXTYPE)
 # release
 #
 release: release-$(FLAVOUR)
+#
+# lib usr/lib
+#
+	cp -R $(TARGET_DIR)/lib/* $(RELEASE_DIR)/lib/
+	rm -f $(RELEASE_DIR)/lib/*.{a,o,la}
+	chmod 755 $(RELEASE_DIR)/lib/*
+
+	cp -R $(TARGET_DIR)/usr/lib/* $(RELEASE_DIR)/usr/lib/
+
+	rm -rf $(RELEASE_DIR)/usr/lib/{engines,gconv,libxslt-plugins,pkgconfig,sigc++-2.0,python*,lua}
+	rm -f $(RELEASE_DIR)/usr/lib/*.{a,o,la}
+
+	chmod 755 $(RELEASE_DIR)/usr/lib/*
+
+#
+# delete unnecessary files
+#
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), ufs910 ufs922))
+	rm -f $(RELEASE_DIR)/sbin/jfs_fsck
+	rm -f $(RELEASE_DIR)/sbin/fsck.jfs
+	rm -f $(RELEASE_DIR)/sbin/jfs_mkfs
+	rm -f $(RELEASE_DIR)/sbin/mkfs.jfs
+	rm -f $(RELEASE_DIR)/sbin/jfs_tune
+	rm -f $(RELEASE_DIR)/sbin/ffmpeg
+	rm -f $(RELEASE_DIR)/etc/ssl/certs/ca-certificates.crt
+endif
+	rm -rf $(RELEASE_DIR)/lib/autofs
+	rm -f $(RELEASE_DIR)/lib/libSegFault*
+	rm -f $(RELEASE_DIR)/lib/libstdc++.*-gdb.py
+	rm -f $(RELEASE_DIR)/lib/libthread_db*
+	rm -f $(RELEASE_DIR)/lib/libanl*
+	rm -rf $(RELEASE_DIR)/lib/modules/$(KERNEL_VER)
+	rm -rf $(RELEASE_DIR)/usr/lib/alsa
+	rm -rf $(RELEASE_DIR)/usr/lib/glib-2.0
+	rm -rf $(RELEASE_DIR)/usr/lib/cmake
+	rm -f $(RELEASE_DIR)/usr/lib/*.py
+	rm -f $(RELEASE_DIR)/usr/lib/libc.so
+	rm -f $(RELEASE_DIR)/usr/lib/xml2Conf.sh
+	rm -f $(RELEASE_DIR)/usr/lib/libfontconfig*
+#	rm -f $(RELEASE_DIR)/usr/lib/libdvdcss*
+#	rm -f $(RELEASE_DIR)/usr/lib/libdvdnav*
+#	rm -f $(RELEASE_DIR)/usr/lib/libdvdread*
+	rm -f $(RELEASE_DIR)/usr/lib/libcurses.so
+	[ ! -e $(RELEASE_DIR)/usr/bin/mc ] && rm -f $(RELEASE_DIR)/usr/lib/libncurses* || true
+	rm -f $(RELEASE_DIR)/usr/lib/libthread_db*
+	rm -f $(RELEASE_DIR)/usr/lib/libanl*
+	rm -f $(RELEASE_DIR)/usr/lib/libopkg*
+	rm -f $(RELEASE_DIR)/bin/gitVCInfo
+	rm -f $(RELEASE_DIR)/bin/evtest
+	rm -f $(RELEASE_DIR)/bin/meta
+	rm -f $(RELEASE_DIR)/bin/streamproxy
+#	rm -f $(RELEASE_DIR)/bin/libstb-hal-test
+	rm -f $(RELEASE_DIR)/sbin/ldconfig
+#	rm -f $(RELEASE_DIR)/usr/bin/pic2m2v
+	rm -f $(RELEASE_DIR)/usr/bin/{gdbus-codegen,glib-*,gtester-report}
+ifeq ($(BOXARCH), $(filter $(BOXARCH), arm mips))
+	rm -rf $(RELEASE_DIR)/dev.static
+	rm -rf $(RELEASE_DIR)/ram
+	rm -rf $(RELEASE_DIR)/root
+endif
+
+#
+#
+#	
 ifneq ($(OPTIMIZATIONS), $(filter $(OPTIMIZATIONS), kerneldebug debug normal))
 	find $(RELEASE_DIR)/ -name '*' -exec $(TARGET)-strip --strip-unneeded {} &>/dev/null \;
 endif
--- make/titan.mk
+++ make/titan.mk
@@ -81,15 +81,14 @@ endif
 
 ifeq ($(BOXARCH), sh4)
 T_CPPFLAGS   += -DSH4
+else
+T_CPPFLAGS   += -DMIPSEL
 endif
 
 #
 #
 #
 MACHINE := $(BOXTYPE)
-ifeq ($(BOXARCH), arm)
-MACHINE = hd51
-endif
 ifeq ($(BOXARCH), mips)
 MACHINE = vuduo
 endif
@@ -271,67 +270,9 @@ titan-plugins-distclean:
 #
 release-TITAN: release-NONE $(D)/titan
 	cp -af $(TARGET_DIR)/usr/local/bin $(RELEASE_DIR)/usr/local/
-	cp -aR $(SOURCE_DIR)/titan/skins/0acht5zehn $(RELEASE_DIR)/usr/local/share/skin
-	
-#
-# lib usr/lib
-#
-	cp -R $(TARGET_DIR)/lib/* $(RELEASE_DIR)/lib/
-	rm -f $(RELEASE_DIR)/lib/*.{a,o,la}
-	chmod 755 $(RELEASE_DIR)/lib/*
-
-	cp -R $(TARGET_DIR)/usr/lib/* $(RELEASE_DIR)/usr/lib/
-
-	rm -rf $(RELEASE_DIR)/usr/lib/{engines,gconv,libxslt-plugins,pkgconfig,sigc++-2.0,python*,lua}
-	rm -f $(RELEASE_DIR)/usr/lib/*.{a,o,la}
-
-	chmod 755 $(RELEASE_DIR)/usr/lib/*
-	
-#
-# delete unnecessary files
-#
-ifeq ($(BOXTYPE), $(filter $(BOXTYPE), ufs910 ufs922))
-	rm -f $(RELEASE_DIR)/sbin/jfs_fsck
-	rm -f $(RELEASE_DIR)/sbin/fsck.jfs
-	rm -f $(RELEASE_DIR)/sbin/jfs_mkfs
-	rm -f $(RELEASE_DIR)/sbin/mkfs.jfs
-	rm -f $(RELEASE_DIR)/sbin/jfs_tune
-	rm -f $(RELEASE_DIR)/etc/ssl/certs/ca-certificates.crt
-endif
-	rm -rf $(RELEASE_DIR)/lib/autofs
-	rm -f $(RELEASE_DIR)/lib/libSegFault*
-	rm -f $(RELEASE_DIR)/lib/libstdc++.*-gdb.py
-	rm -f $(RELEASE_DIR)/lib/libthread_db*
-	rm -f $(RELEASE_DIR)/lib/libanl*
-	rm -rf $(RELEASE_DIR)/lib/modules/$(KERNEL_VER)
-	rm -rf $(RELEASE_DIR)/usr/lib/alsa
-	rm -rf $(RELEASE_DIR)/usr/lib/glib-2.0
-	rm -rf $(RELEASE_DIR)/usr/lib/cmake
-	rm -f $(RELEASE_DIR)/usr/lib/*.py
-	rm -f $(RELEASE_DIR)/usr/lib/libc.so
-	rm -f $(RELEASE_DIR)/usr/lib/xml2Conf.sh
-	rm -f $(RELEASE_DIR)/usr/lib/libfontconfig*
-	rm -f $(RELEASE_DIR)/usr/lib/libdvdcss*
-	rm -f $(RELEASE_DIR)/usr/lib/libdvdnav*
-	rm -f $(RELEASE_DIR)/usr/lib/libdvdread*
-	rm -f $(RELEASE_DIR)/usr/lib/libcurses.so
-	[ ! -e $(RELEASE_DIR)/usr/bin/mc ] && rm -f $(RELEASE_DIR)/usr/lib/libncurses* || true
-	rm -f $(RELEASE_DIR)/usr/lib/libthread_db*
-	rm -f $(RELEASE_DIR)/usr/lib/libanl*
-	rm -f $(RELEASE_DIR)/usr/lib/libopkg*
-	rm -f $(RELEASE_DIR)/bin/gitVCInfo
-	rm -f $(RELEASE_DIR)/bin/evtest
-	rm -f $(RELEASE_DIR)/bin/meta
-	rm -f $(RELEASE_DIR)/bin/streamproxy
-	rm -f $(RELEASE_DIR)/bin/libstb-hal-test
-	rm -f $(RELEASE_DIR)/sbin/ldconfig
-	rm -f $(RELEASE_DIR)/usr/bin/pic2m2v
-	rm -f $(RELEASE_DIR)/usr/bin/{gdbus-codegen,glib-*,gtester-report}
-ifeq ($(BOXARCH), $(filter $(BOXARCH), arm mips))
-	rm -rf $(RELEASE_DIR)/dev.static
-	rm -rf $(RELEASE_DIR)/ram
-	rm -rf $(RELEASE_DIR)/root
-endif	
+	cp -aR $(SOURCE_DIR)/titan/skins/default $(RELEASE_DIR)/var/usr/local/share/titan/skin
+	cp -af $(SKEL_ROOT)/var/etc/titan $(RELEASE_DIR)/var/etc/
+	cp -af $(SKEL_ROOT)/var/usr/share/fonts $(RELEASE_DIR)/var/usr/share
 	
 #
 #
