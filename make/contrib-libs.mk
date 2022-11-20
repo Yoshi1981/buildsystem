df6d572170e142a9bbca247aa9f8ae441467ee94
mohousch <mohousch@gmail.com>
2022-11-20 22:14:26 +0100

    - fix graphlcd download.

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -1669,10 +1669,14 @@ $(D)/pugixml: $(D)/bootstrap $(ARCHIVE)/$(PUGIXML_SOURCE)
 #
 GRAPHLCD_VER = 55d4bd8
 GRAPHLCD_SOURCE = graphlcd-git-$(GRAPHLCD_VER).tar.bz2
-GRAPHLCD_URL = https://projects.vdr-developer.org/git/graphlcd-base.git
+#GRAPHLCD_URL = https://projects.vdr-developer.org/git/graphlcd-base.git
+GRAPHLCD_URL = https://github.com/Duckbox-Developers/graphlcd.git
 GRAPHLCD_PATCH = graphlcd-git-$(GRAPHLCD_VER).patch
-ifeq ($(BOXTYPE), vusolo4k)
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), vuduo4k vuduo4kse vuuno4kse vuultimo4k vusolo4k))
 GRAPHLCD_PATCH += graphlcd-vusolo4k.patch
+endif
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), e4hdultra))
+GRAPHLCD_PATCH += graphlcd-e4hdultra.patch
 endif
 
 $(ARCHIVE)/$(GRAPHLCD_SOURCE):
